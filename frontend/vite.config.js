73be3e808e5f3b4eca32031ad3e591023283f6b7
Tim Yim <airyimbin@gmail.com>
2025-10-30 16:23:21 -0400

    asdf

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,6 +5,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: "0.0.0.0",
+    allowedHosts: ['projectfinal.timyim.info'],
     port: 5173,
     proxy: {
       "/api": { target: "http://localhost:3000", changeOrigin: true }
